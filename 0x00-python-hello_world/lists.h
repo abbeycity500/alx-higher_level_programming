0b5651a10ca0208da743426045ec98ac397d620e
abbeycity500 <wahababiodun766@gmail.com>
2023-11-29 14:17:28 +0100

    Python - Hello, World

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,25 +1,26 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdlib.h>
+#include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
  *
- * Description: singly linked list node structure
- * 
+ * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-typedef struct listint_s
+int check_cycle(listint_t *list)
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t *head);
-int check_cycle(listint_t *list);
+	if (!list)
+		return (0);
 
-#endif /* LISTS_H */
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
