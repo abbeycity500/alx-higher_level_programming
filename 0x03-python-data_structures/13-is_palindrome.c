85ec2f196e55e32d2a83b5581194e0754042f142
abbeycity500 <wahababiodun766@gmail.com>
2023-12-05 09:38:36 -0800

    Hot fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,74 +1,26 @@
-/*
- * File: 13-is_palindrome.c
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
  */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
 
-#include "lists.h"
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
-listint_t *reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
 
-/**
- * reverse_listint - Reversing a singly-linked listint_t list.
- * @head: A pointer to the starting node of the list to reverse.
- * Return: A pointer to the head of the reversed list.
- */
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *node = *head, *next, *prev = NULL;
-
-	while (node)
-	{
-		next = node->next;/** Node next */
-		node->next = prev;
-		prev = node;/** prev node */
-		node = next;
-	}
-
-	*head = prev;
-	return (*head);
-}
-
-/**
- * is_palindrome - Checking if a singly linked list is a palindrome.
- * @head: A pointer to the head of the linked list.
- * Return: If the linked list is not a palindrome - 0.
- *         If the linked list is a palindrome - 1.
- */
-int is_palindrome(listint_t **head)
-{
-	listint_t *tmp, *rev, *mid;
-	size_t size = 0, i;/** size t */
-
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);/** returning 1 */
-
-	tmp = *head;
-	while (tmp)
-	{
-		size++;
-		tmp = tmp->next;/** next tmp */
-	}
-
-	tmp = *head;
-	for (i = 0; i < (size / 2) - 1; i++)
-		tmp = tmp->next;/** next tmp */
-
-	if ((size % 2) == 0 && tmp->n != tmp->next->n)
-		return (0);/** returning 0 */
-
-	tmp = tmp->next->next;
-	rev = reverse_listint(&tmp);/** listint reverse */
-	mid = rev;
-
-	tmp = *head;
-	while (rev)
-	{
-		if (tmp->n != rev->n)
-			return (0);
-		tmp = tmp->next;/** tmp next */
-		rev = rev->next;
-	}
-	reverse_listint(&mid);/** listing mid */
-
-	return (1);
-}
+#endif /* LISTS_H */
