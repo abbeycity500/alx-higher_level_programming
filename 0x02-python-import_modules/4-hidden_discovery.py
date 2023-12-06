c45e11d493d9cc7b8dfc1a3fc1e67ce771be27fe
abbeycity500 <wahababiodun766@gmail.com>
2023-12-06 13:03:33 -0800

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
+import hidden_4
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    list = []
+    for i in dir(hidden_4):
+        if i[0] != '_':
+            list.append(i)
+    for i in list:
+        print(i)
