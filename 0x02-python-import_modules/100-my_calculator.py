3833a35a2397e227e300e1c549e2040f7161e573
abbeycity500 <wahababiodun766@gmail.com>
2023-12-06 13:07:16 -0800

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-from sys import argv
-"""Handle basic arithmetic operations."""
-from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
-        exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    op = argv[2]
-    ops = {
-            "+": add(a, b),
-            "-": sub(a, b),
-            "*": mul(a, b),
-            "/": div(a, b)}
-    if op in ops.keys():
-        print("{:d} {} {:d} = {:d}".format(a, op, b, ops[op]))
-    else:
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div 
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
