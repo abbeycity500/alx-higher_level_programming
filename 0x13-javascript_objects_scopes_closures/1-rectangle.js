296d450dad6a5b69d1e486b9e8bfe2d1ebc6daa4
abbeycity500 <wahababiodun766@gmail.com>
2024-03-12 21:30:55 +0100

    Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-module.export = class Rectangle {
+module.exports = class Rectangle {
 	constructor (w, h) {
 		this.width = w;
 		this.height = h;
--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 
 module.exports = class Rectangle {
 	constructor (w, h) {
-		if ( h > 0 && w > 0 ) {
+		if (h > 0 && w > 0) {
 			this.width = w;
 			this.height = h;
 		}
